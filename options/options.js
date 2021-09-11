9779bc2171151e5df55ca8c1122d5cdef0013646
Crowdbotics <team@crowdbotics.com>
2021-09-11 12:40:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "xawsgroup_30467Identifier",
+  url: "https://xawsgroup_30467Identifier.botics.co",
+  api: "https://xawsgroup_30467Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
